82cf4a35d879307e086c01ee82ff8b578cdc8a00
sureshkumar <drop2vsureshkumarplay@gmail.com>
2021-09-08 14:04:26 +0530

    Update Two Sum.py
    add command lines

--- Two Sum.py
+++ Two Sum.py
@@ -28,8 +28,8 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i+1 , len(nums)):
+        for i in range(len(nums)): # Here we fix the 1st numbers
+            for j in range(i+1 , len(nums)): # iterate throuth other numbers
                 if nums[i] + nums[j] == target:
                     return [i,j]
 
